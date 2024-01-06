c79f2f34520026b041b2d6d8041f537c389f9a0b
Miroslav Sikora <miroslavsikora5@gmail.com>
2024-01-06 09:49:11 +0100

    Weather in all cities. Close #1

--- src/main/java/cz/vsb/project/controller/WeatherController.java
+++ src/main/java/cz/vsb/project/controller/WeatherController.java
@@ -8,13 +8,29 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.net.URISyntaxException;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
 
 @RestController
 public class WeatherController {
+    private final WeatherService weatherService;
+    public WeatherController(WeatherService weatherService) {
+        this.weatherService = weatherService;
+    }
+
+    @RequestMapping("/weather")
+    public Collection<WeatherDto> getWeather() throws URISyntaxException {
+        List<WeatherDto> weatherDtoList = new ArrayList<>();
+        for(City city:City.values()){
+            weatherDtoList.add(weatherService.getWeatherForCity(city));
+        }
+        return weatherDtoList;
+    }
+
     @RequestMapping("/weather/{city}")
     public WeatherDto getWeatherForCity(@PathVariable ("city") String city) throws URISyntaxException {
         City cityEnum = City.valueOf(city.toUpperCase());
-        WeatherService weatherService = new WeatherService();
         return weatherService.getWeatherForCity(cityEnum);
     }
 
